726399d8aa4354a053a553bc420919f55b85b9c1
Alrighttt <mzlot555@gmail.com>
2018-12-08 19:02:01 +0100

    begin rework

--- genaddresses.py
+++ genaddresses.py
@@ -1,48 +1,23 @@
 #!/usr/bin/env python3
 import json
 import requests
-from config import *
+import sys
+import kmdrpc
 
-rpcurl = (
-    'http://' +
-    rpcuser +
-    ':' +
-    rpcpassword +
-    '@' +
-    rpcip +
-    ':' +
-    rpcport)
-
-
-# define function that posts json data
-def post_rpc(url, payload, auth=None):
-    try:
-        r = requests.post(url, data=json.dumps(payload), auth=auth)
-        return(json.loads(r.text))
-    except Exception as e:
-        raise Exception("Couldn't connect to " + url + ": ", e)
-
+CHAIN = sys.argv[1]
 
 # generate address, validate address, dump private key
 def genvaldump():
     # get new address
-    payload = {"method": "getnewaddress"}
-    getnewaddress_result = post_rpc(rpcurl, payload)
+    getnewaddress_result = kmdrpc.getnewaddress_rpc(CHAIN)
     address = getnewaddress_result['result']
     # validate address
-    payload = {
-        "method": "validateaddress",
-        "params": [address]}
-    validateaddress_result = post_rpc(rpcurl, payload)
+    validateaddress_result = kmdrpc.validateaddress_rpc(CHAIN, address)
     segid = validateaddress_result['result']['segid']
     pubkey = validateaddress_result['result']['pubkey']
     address = validateaddress_result['result']['address']
     # dump private key for the address
-    payload = {
-        "method": "dumpprivkey",
-        "params": [address]
-    }
-    dumpprivkey_result = post_rpc(rpcurl, payload)
+    dumpprivkey_result = kmdrpc.dumpprivkey_rpc(CHAIN, address)
     privkey = dumpprivkey_result['result']
     # function output
     output = [segid, pubkey, privkey, address]
@@ -53,6 +28,7 @@ def genvaldump():
 segids = {}
 while len(segids.keys()) < 64:
     genvaldump_result = genvaldump()
+    print(genvaldump_result)
     segid = genvaldump_result[0]
     if segid in segids:
         pass
@@ -64,5 +40,6 @@ segids_array = []
 for position in range(64):
     segids_array.append(segids[position])
 
-# print output
-print("segids = " + str(json.dumps(segids_array)))
+# save output to list.py
+f = open("list.py","w+")
+f.write("segids = " + str(json.dumps(segids_array)))
---
+++ kmdrpc.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+import re
+import os
+import requests
+import json
+import platform
+
+# define function that fetchs rpc creds from .conf
+def def_credentials(chain):
+    operating_system = platform.system()
+    if operating_system == 'Darwin':
+        ac_dir = os.environ['HOME'] + '/Library/Application Support/Komodo'
+    elif operating_system == 'Linux':
+        ac_dir = os.environ['HOME'] + '/.komodo'
+    elif operating_system == 'Win64':
+        ac_dir = "dont have windows machine now to test"
+    # define config file path
+    if chain == 'KMD':
+        coin_config_file = str(ac_dir + '/komodo.conf')
+    else:
+        coin_config_file = str(ac_dir + '/' + chain + '/' + chain + '.conf')
+    #define rpc creds
+    with open(coin_config_file, 'r') as f:
+        #print("Reading config file for credentials:", coin_config_file)
+        for line in f:
+            l = line.rstrip()
+            if re.search('rpcuser', l):
+                rpcuser = l.replace('rpcuser=', '')
+            elif re.search('rpcpassword', l):
+                rpcpassword = l.replace('rpcpassword=', '')
+            elif re.search('rpcport', l):
+                rpcport = l.replace('rpcport=', '')
+    return('http://' + rpcuser + ':' + rpcpassword + '@127.0.0.1:' + rpcport)
+
+# define function that posts json data
+def post_rpc(url, payload, auth=None):
+    try:
+        r = requests.post(url, data=json.dumps(payload), auth=auth)
+        return(json.loads(r.text))
+    except Exception as e:
+        raise Exception("Couldn't connect to " + url + ": ", e)
+
+# VANILLA RPC
+
+def sendrawtx_rpc(chain, rawtx):
+    sendrawtx_payload = {
+        "jsonrpc": "1.0",
+        "id": "python",
+        "method": "sendrawtransaction",
+        "params": [rawtx]}
+    return(post_rpc(def_credentials(chain), sendrawtx_payload))
+
+def validateaddress_rpc(chain, address):
+    validateaddress_payload = {
+        "jsonrpc": "1.0",
+        "id": "python",
+        "method": "validateaddress",
+        "params": [address]}
+    return(post_rpc(def_credentials(chain), validateaddress_payload))
+
+def dumpprivkey_rpc(chain, address):
+    dumpprivkey_payload = {
+        "jsonrpc": "1.0",
+        "id": "python",
+        "method": "dumpprivkey",
+        "params": [address]}
+    return(post_rpc(def_credentials(chain), dumpprivkey_payload))
+
+def getnewaddress_rpc(chain):
+    getnewaddress_payload = {
+        "jsonrpc": "1.0",
+        "id": "python",
+        "method": "getnewaddress",
+        "params": []}
+    return(post_rpc(def_credentials(chain), getnewaddress_payload))
