4a3f009add535d059a120bdafd565ef88e56227a
Alrighttt <mzlot555@gmail.com>
2018-12-09 17:29:05 +0100

    change importlist to json

--- importlist.py
+++ importlist.py
@@ -2,13 +2,15 @@
 import os
 import sys
 import kmdrpc
+import json
 
-if not os.path.isfile("list.py"):
-    sys.exit('No list.py file present. Use genaddresses.py script to generate one.')
+if not os.path.isfile("list.json"):
+    sys.exit('No list.json file present. Use genaddresses.py script to generate one.')
 
-from list import segids
+CHAIN = input('Please specify chain to import list.json keys to:')
 
-CHAIN = input('Please specify chain to import list.py keys to:')
-
-for e in segids:
-    kmdrpc.importprivkey_rpc(CHAIN, e[2])
+with open('list.json') as key_list:
+    json_data = json.load(key_list)
+    for i in json_data:
+        print(i[3])
+        kmdrpc.importprivkey_rpc(CHAIN, i[2])
