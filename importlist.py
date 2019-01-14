9684e22fb5d5a94506c5f56e120894f98677ea4e
blackjok3r <blackjok3r@gmail.com>
2019-01-14 14:29:58 +0800

    fix stakerlib importlist.py

--- importlist.py
+++ importlist.py
@@ -2,18 +2,17 @@
 import os
 import sys
 import json
-from stakerlib import def_credentials
+import stakerlib
 
 if not os.path.isfile("list.json"):
     sys.exit('No list.json file present. Use genaddresses.py script to generate one.')
 
 CHAIN = input('Please specify chain to import list.json keys to:')
 
-rpc_connection = def_credentials(CHAIN)
+rpc_connection = stakerlib.def_credentials(CHAIN)
 
 with open('list.json') as key_list:
     json_data = json.load(key_list)
     for i in json_data:
         print(i[3])
         rpc_connection.importprivkey(i[2])
-
