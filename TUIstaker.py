50dea06c087170261a62e1f966c6584a86b1f50c
Alrighttt <mzlot555@gmail.com>
2019-01-27 23:56:42 +0100

    start TUI-izing

---
+++ TUIstaker.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+from slickrpc import Proxy
+import json
+import sys
+import stakerlib
+
+def selectRangeInt(low,high, msg):
+    while True:
+        user_input = input(msg)
+        if user_input == ('q' or 'quit'):
+            print('Exiting...')
+            sys.exit(0)
+        try:
+            number = int(user_input)
+        except ValueError:
+            print("integer only, try again")
+            continue
+        if low <= number <= high:
+            return number
+        else:
+            print("input outside range, try again")
+
+def selectRangeFloat(low,high, msg):
+    while True:
+        try:
+            number = float(user_input)
+        except ValueError:
+            print("integer only, try again")
+            continue
+        if low <= number <= high:
+            return number
+        else:
+            print("input outside range, try again")
+
+def load_conf():
+    try:
+        with open('staker.conf') as file:
+            staker_conf = json.load(file)
+    except Exception as e:
+        print(e)
+        staker_conf = []
+        user_input = input('No staker.conf conf file, specify a chain to begin:')
+        staker_conf.append([user_input])
+        with open('staker.conf', "a+") as f:
+            json.dump([staker_conf], f)
+    return(staker_conf)
+
+def initial_menu(staker_conf):
+    print('\n')
+    menu_item = 0
+    for i in staker_conf:
+        print(str(menu_item) + ' | ' + str(i[0]))
+        menu_item += 1
+    print(str(len(staker_conf)) + ' | <Add/remove chain>\n')
+    print('q | Exit TUI\n')
+
+def print_menu(menu_list):
+    print('\n')
+    menu_item = 0
+    for i in menu_list:
+        print(str(menu_item) + ' | ' + str(i))
+        menu_item += 1
+    print(str(len(menu_list)) + ' | <return to previous menu>\n')
+    print('q | Exit TUI\n')
+
+def select_loop():
+    staker_conf = load_conf()
+    initial_menu(staker_conf)
+    chain_index = selectRangeInt(0,len(staker_conf),"Select chain:")
+    while True:
+        if chain_index == len(staker_conf):
+            new_chain = input('Add/remove chain:')
+            # delete and restart loop if user inputted chain exists in conf
+            for i in (range(len(staker_conf))):
+                if staker_conf[i][0] ==  new_chain:
+                    del staker_conf[i]
+                    with open('staker.conf', mode='w', encoding='utf-8') as f:
+                        json.dump(staker_conf, f)
+                    select_loop()
+            # add new chain to conf, stored as a list in case we want to save something, dummy value for now
+            staker_conf.append([new_chain, 1])
+            with open('staker.conf', mode='w', encoding='utf-8') as f:
+                json.dump(staker_conf, f)
+            initial_menu(staker_conf)
+            chain_index = selectRangeInt(0,len(staker_conf),"Select chain:")
+        else:
+            #chain_index = selectRangeInt(0,len(staker_conf),"Select chaineee:")
+            chain = staker_conf[chain_index][0]
+            chain_loop(chain)
+
+# Chain menu, to add additional options, add what will be displayed to chain_menu
+# and an elif for it's position in the list
+def chain_loop(chain):
+    try:    
+        rpc_connection = stakerlib.def_credentials(chain)
+    except:
+        print('Could not connect to daemon. ' + chain + ' is not running or rpc creds not found.')
+        select_loop()
+    while True:
+        print_menu(chain_menu)
+        selection = selectRangeInt(0,len(chain_menu),"make a selection:")
+        if int(selection) == len(chain_menu):
+            select_loop()
+        elif int(selection) == 0:
+            stakerlib.sendmany64_TUI(chain)
+        elif int(selection) == 1:
+            stakerlib.RNDsendmany_TUI(chain)
+        elif int(selection) == 2:
+            stakerlib.genaddresses(chain)
+        elif int(selection) == 3:
+            stakerlib.import_list(chain)
+        elif int(selection) == 4:
+            print('with not implemented')
+        elif int(selection) == 5:
+            print('stats not implemented yet')
+        else:
+            print('BUG!')
+
+chain_menu = ['sendmany64','RNDsendmany', 'genaddresses', 'importlist', 'withdraw', 'stats']
+select_loop()
+
+
+
--- stakerlib.py
+++ stakerlib.py
@@ -2,6 +2,7 @@ import platform
 import os
 import re
 import json
+import random
 from slickrpc import Proxy
 
 
@@ -82,3 +83,185 @@ def sendmany64(rpc_connection, amount):
     # make rpc call, issue transaction
     sendmany_result = rpc_connection.sendmany("", addresses_dict)
     return(sendmany_result)
+
+# function to do sendmany64 UTXOS times, locking all UTXOs except change
+def sendmanyloop(rpc_connection, amount, utxos):
+    txid_list = []
+    for i in range(int(utxos)):
+        sendmany64_txid = sendmany64(rpc_connection, amount)
+        txid_list.append(sendmany64_txid)
+        getrawtx_result = rpc_connection.getrawtransaction(sendmany64_txid, 1)
+        lockunspent_list = []
+        # find change output, lock all other outputs
+        for vout in getrawtx_result['vout']:
+            if vout['value'] != float(amount):
+                change_output = vout['n']
+            else:
+                output_dict = {
+                    "txid": sendmany64_txid,
+                    "vout": vout['n']
+                    }
+                lockunspent_list.append(output_dict)
+        lockunspent_result = rpc_connection.lockunspent(False, lockunspent_list)
+    return(txid_list)
+
+def sendmany64_TUI(chain):
+    try:
+        rpc_connection = def_credentials(chain)
+    except Exception as e:
+        print(e)
+        return(0)
+    balance = float(rpc_connection.getbalance())
+    print('Balance: ' + str(balance))
+
+    AMOUNT = input("Please specify the size of UTXOs: ")
+    if int(AMOUNT) < 1:
+        print('Cant stake coin amounts less than 1 coin, try again.')
+        return(0)
+    UTXOS = input("Please specify the amount of UTXOs to send to each segid: ")
+    total = float(AMOUNT) * int(UTXOS) * 64
+    print('Total amount: ' + str(total))
+    if total > balance:
+        print('Total sending is ' + str(total-balance) + ' more than your balance. Try again.')
+        segidTotal = balance / 64
+        print('Total avalible per segid is: ' + str(segidTotal))
+        return(0)
+
+    sendmanyloop_result = sendmanyloop(rpc_connection, AMOUNT, UTXOS)
+    # unlock all locked utxos
+    unlockunspent(rpc_connection)
+    for i in sendmanyloop_result:
+        print(i)
+    print('Success!')
+
+# function to do sendmany64 UTXOS times, locking all UTXOs except change
+def RNDsendmanyloop(rpc_connection, amounts):
+    txid_list = []
+    for amount in amounts:
+        sendmany64_txid = sendmany64(rpc_connection, amount)
+        txid_list.append(sendmany64_txid)
+        getrawtx_result = rpc_connection.getrawtransaction(sendmany64_txid, 1)
+        lockunspent_list = []
+        # find change output, lock all other outputs
+        for vout in getrawtx_result['vout']:
+            if vout['value'] != float(amount):
+                change_output = vout['n']
+            else:
+                output_dict = {
+                    "txid": sendmany64_txid,
+                    "vout": vout['n']
+                    }
+                lockunspent_list.append(output_dict)
+        lockunspent_result = rpc_connection.lockunspent(False, lockunspent_list)
+    return(txid_list)
+
+def RNDsendmany_TUI(chain):
+    try:
+        rpc_connection = def_credentials(chain)
+    except Exception as e:
+        print(e)
+        return(0)
+
+    try:
+        balance = float(rpc_connection.getbalance())
+    except Exception as e:
+        print(e)
+        return(0)
+
+    print('Balance: ' + str(balance))
+
+    while True:
+        UTXOS = int(input("Please specify the amount of UTXOs to send to each segid: "))
+        if UTXOS < 3:
+            print('Must have more than 3 utxos per segid, try again.')
+            continue
+        TUTXOS = UTXOS * 64
+        print('Total number of UTXOs: ' + str(TUTXOS))
+        average = float(balance) / int(TUTXOS)
+        print('Average utxo size: ' + str(average))
+        variance = float(input('Enter percentage of variance: '))
+        minsize = round(float(average) * (1-(variance/100)),2)
+        if minsize < 1:
+            print('Cant stake coin amounts less than 1 coin, try again.')
+            continue
+        maxsize = round(average + float(average) * (variance/100),2)
+        print('Min size: ' + str(minsize))
+        print('Max size: ' + str(maxsize))
+        ret = input('Are you happy with these? ').lower()
+        if ret.startswith('y'):
+            break
+
+    total = 0
+    totalamnt = 0
+    AMOUNTS = []
+    finished = False
+
+    while finished == False:
+        for i in range(UTXOS):
+            amnt = round(random.uniform(minsize,maxsize),2)
+            totalamnt += amnt * 64
+            AMOUNTS.append(amnt)
+            if totalamnt > balance-0.1:
+                totalamnt = 0
+                AMOUNTS.clear()
+                break
+        if totalamnt > balance-2:
+            finished = True
+
+    sendmanyloop_result = RNDsendmanyloop(rpc_connection, AMOUNTS)
+    # unlock all locked utxos
+    unlockunspent(rpc_connection)
+    for i in sendmanyloop_result:
+        print(i)
+    print('Success!')
+
+def genaddresses(chain):
+    if os.path.isfile("list.json"):
+        print('Already have list.json, move it if you would like to '
+              'generate another set.You can use importlist.py script to import'
+              ' the already existing list.py to a given chain.')
+        return(0)
+
+    # create rpc_connection
+    try:
+        rpc_connection = def_credentials(chain)
+    except Exception as e:
+        print(e)
+        return(0)
+    
+    # fill a list of sigids with matching segid address data
+    segids = {}
+    while len(segids.keys()) < 64:
+        genvaldump_result = genvaldump(rpc_connection)
+        segid = genvaldump_result[0]
+        if segid in segids:
+            pass
+        else:
+            segids[segid] = genvaldump_result
+
+    # convert dictionary to array
+    segids_array = []
+    for position in range(64):
+        segids_array.append(segids[position])
+
+    # save output to list.py
+    print('Success! list.json created. '
+          'THIS FILE CONTAINS PRIVATE KEYS. KEEP IT SAFE.')
+    f = open("list.json", "w+")
+    f.write(json.dumps(segids_array))
+
+
+# import list.json to chain
+def import_list(chain):
+    if not os.path.isfile("list.json"):
+        print('No list.json file present. Use genaddresses.py script to generate one.')
+        return(0)
+
+    rpc_connection = def_credentials(chain)
+
+    with open('list.json') as key_list:
+        json_data = json.load(key_list)
+        for i in json_data:
+            print(i[3])
+            rpc_connection.importprivkey(i[2])
+    print('Success!')
